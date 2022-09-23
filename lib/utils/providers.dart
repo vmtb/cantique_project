7c4e2ae07c6a3843328a2ea3e04efcec70946b06
princeGedeon <guedjegedeon03@gmail.com>
2022-09-23 23:29:54 +0100

    Mise en place du modele Cantique

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -1,4 +1,9 @@
-import 'package:cantique/controllers/Cantique_crudControlller.dart';
+
+
+import 'dart:io';
+
+import 'package:cantique/models/cantique.dart';
+import 'package:cantique/screens/cantique/cantique_list.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
