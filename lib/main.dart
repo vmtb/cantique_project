60b33edc214b40e91986ce97d15590fe9b276010
ALLOWAKOU Ferdinand <allowakouferdinand@gmail.com>
2022-09-29 18:22:26 +0100

    maj

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:cantique/screens/admin_home_page.dart';
 import 'package:cantique/utils/app_styles.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'screens/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
